ef7e21b6fd5bf15cfd0ed3c231706afbbf2f43f0
Raven Glenn Lagmay <137616076+aconite12@users.noreply.github.com>
2024-01-17 11:27:13 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -50,6 +50,7 @@ const app = express();
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+app.use(helmet.hidePoweredBy());
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
